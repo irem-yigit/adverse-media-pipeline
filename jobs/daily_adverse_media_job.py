3acb0cfb79b6d172056bb4b37ca39c98f0a98b4e
irem-yigit <yigitiremm@gmail.com>
2026-02-06 02:24:23 +0300

    feat: add daily adverse media job and BBC pipeline functionality

---
+++ jobs/daily_adverse_media_job.py
@@ -0,0 +1,9 @@
+from pipeline.bbc_pipeline import run_bbc_pipeline
+
+def run_daily_job():
+    print("Daily adverse media job started")
+    run_bbc_pipeline()
+    print("Daily adverse media job finished")
+
+if __name__ == "__main__":
+    run_daily_job()
