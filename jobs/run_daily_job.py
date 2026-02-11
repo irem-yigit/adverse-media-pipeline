993ca6be6b855197d0043984f25b77e82990fec3
irem-yigit <yigitiremm@gmail.com>
2026-02-11 17:35:26 +0300

    refactor: add log info in run_daily_job folder

--- jobs/run_daily_job.py
+++ jobs/run_daily_job.py
@@ -1,6 +1,9 @@
 from pipeline.bbc_pipeline import run_bbc_pipeline
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 def run_daily_job():
-    print("Daily adverse media job started")
+    logging.info("Daily adverse media job started")
     run_bbc_pipeline()
-    print("Daily adverse media job finished")
+    logging.info("Daily adverse media job finished")
